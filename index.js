f81c296b97d6631e3da8e34e5787595521319985
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-01 21:53:41 +0300

    feat: array lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+var cats = ['Milo','Otis','Garfield']
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    return cats;
+ }
+ 
+ function destructivelyPrependCat() {
+   cats.unshift("Bob");
+   return cats;
+ }
+ 
+ function destructivelyRemoveLastCat() {
+   cats.pop();
+   return cats
+ }
+ 
+ function destructivelyRemoveFirstCat() {
+   cats.shift();
+   return cats;
+ }
+ 
+ function appendCat(){
+   var cats_1 = [...cats,"Broom"];
+   return cats_1;
+ }
+ 
+ function prependCat() {
+   var cats_2=["Arnold", ...cats];
+   return cats_2
+ }
+ 
+ function removeLastCat() {
+    return cats.slice(0, - 1);
+ }
+ 
+ function removeFirstCat() {
+   return cats.slice(1);
+ }
